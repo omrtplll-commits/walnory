4afc9bfdf278041f6a4f26ac5e6c1f636d169ca7
omer <omrtplll@gmail.com>
2026-05-17 15:10:16 +0300

    restore landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
 import LandingPage from "./pages/LandingPage";
-import WeddingPage from "./pages/WeddingPage";
-import OwnerGallery from "./pages/OwnerGallery";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<LandingPage />}
-        />
-
-        <Route
-          path="/welcome/:token"
-          element={<WeddingPage />}
-        />
-
-        <Route
-          path="/gallery/:token"
-          element={<OwnerGallery />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <LandingPage />;
 }
 
 export default App;
