85b64c08753b42b68c73ff05906d769b6bd443c6
omer <omrtplll@gmail.com>
2026-05-26 23:35:46 +0300

    fix csv semicolon

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -106,9 +106,9 @@ function AdminPanel() {
   };
 
   const exportCSV = () => {
-    let csv = "Token\tPackage\tUsed\tUsedBy\tUsedAt\r\n";
+    let csv = "Token;Package;Used;UsedBy;UsedAt\r\n";
     allTokens.forEach((t) => {
-      csv += `${t.id}\t${t.package || "basic"}\t${t.used}\t${t.usedBy || ""}\t${t.usedAt ? new Date(t.usedAt.seconds * 1000).toLocaleDateString() : ""}\r\n`;
+      csv += `${t.id};${t.package || "basic"};${t.used ? "YES" : "NO"};${t.usedBy || ""};${t.usedAt ? new Date(t.usedAt.seconds * 1000).toLocaleDateString() : ""}\r\n`;
     });
     const blob = new Blob(["\uFEFF" + csv], { type: "text/csv;charset=utf-8;" });
     const url = URL.createObjectURL(blob);
